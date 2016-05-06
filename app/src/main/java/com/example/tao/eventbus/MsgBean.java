76af10b87e0f087fd9c63f6f07619460e3ad749b
tchl <whtchl@126.com>
2016-05-06 08:02:26 +0800

    init project

---
+++ app/src/main/java/com/example/tao/eventbus/MsgBean.java
@@ -0,0 +1,20 @@
+package com.example.tao.eventbus;
+
+/**
+ * Created by tao on 2016/5/6.
+ */
+public class MsgBean {
+    private String msg;
+
+    public MsgBean(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
